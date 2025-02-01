7dd8669851f035365fdb701b6c9951c2b57afcfc
zero <teclaizerai@gmail.com>
2025-02-01 20:41:41 +0100

    Conf. application modules.

--- api/phparkitect.php
+++ api/phparkitect.php
@@ -81,4 +81,90 @@ return static function (Config $config): void {
 
     $config->add($cartContextClassSet, $cartCoreIsolationRule, $cartAdaptersIsolationRule, ...$cartContextPortAndAdapterArchitectureRules);
 
+
+
+    /**++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+     *
+     *      INVENTORY CONTEXT
+     *
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+     */
+    $inventoryContextClassSet = ClassSet::fromDir(__DIR__ . '/context/inventory/src');
+
+    $allowedPhpDependencies = require_once __DIR__ . '/tools/ark/config/inventory/allowed_php_deps_in_core.php';
+    $allowedVendorDependenciesInInventoryContextCore = require_once __DIR__ . '/tools/ark/config/inventory/allowed_vendor_deps_in_core.php';
+    $allowedVendorDependenciesInInventoryContextAdapters = require_once __DIR__ . '/tools/ark/config/inventory/allowed_vendor_deps_in_adapters.php';
+
+    $inventoryContextPortAndAdapterArchitectureRules = Architecture::withComponents()
+        ->component('Core')->definedBy('Inventory\Core\*')
+        ->component('Adapters')->definedBy('Inventory\AdapterFor*')
+        ->component('Infrastructure')->definedBy('Inventory\Infrastructure\*')
+
+        ->where('Infrastructure')->mayDependOnComponents('Core')
+        ->where('Adapters')->mayDependOnComponents('Core', 'Infrastructure')
+        ->where('Core')->shouldNotDependOnAnyComponent()
+        ->rules();
+
+
+    $allowedDependenciesInInventoryContextCoreCode = array_merge($allowedPhpDependencies, $allowedVendorDependenciesInInventoryContextCore);
+    $inventoryCoreIsolationRule = Rule::allClasses()
+        ->that(new ResideInOneOfTheseNamespaces('Inventort\Core'))
+        ->should(new NotHaveDependencyOutsideNamespace('Inventort\Core', $allowedDependenciesInInventoryContextCoreCode))
+        ->because('we want isolate our inventory core domain from external world.');
+
+
+    $allowedDependenciesInInventoryContextAdapters = array_merge($allowedPhpDependencies, $allowedVendorDependenciesInInventoryContextAdapters);
+    $inventoryAdaptersIsolationRule = Rule::allClasses()
+        //->except('Inventory\Adapter\Http\Web\FooController', 'Inventory\Adapter\Packagist\FooApiAdapter')
+        ->that(new ResideInOneOfTheseNamespaces('Inventory\Adapter*'))
+        ->should(new NotHaveDependencyOutsideNamespace('Inventory\Core', $allowedDependenciesInInventoryContextAdapters))
+        ->because('we want isolate our inventory Adapters from ever growing dependencies.');
+
+    $config->add($inventoryContextClassSet, $inventoryCoreIsolationRule, $inventoryAdaptersIsolationRule, ...$inventoryContextPortAndAdapterArchitectureRules);
+
+
+
+
+    /**++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+     *
+     *      PRICING CONTEXT
+     *
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+     *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+     */
+    $pricingContextClassSet = ClassSet::fromDir(__DIR__ . '/context/pricing/src');
+
+    $allowedPhpDependencies = require_once __DIR__ . '/tools/ark/config/pricing/allowed_php_deps_in_core.php';
+    $allowedVendorDependenciesInPricingContextCore = require_once __DIR__ . '/tools/ark/config/pricing/allowed_vendor_deps_in_core.php';
+    $allowedVendorDependenciesInPricingContextAdapters = require_once __DIR__ . '/tools/ark/config/pricing/allowed_vendor_deps_in_adapters.php';
+
+    $pricingContextPortAndAdapterArchitectureRules = Architecture::withComponents()
+        ->component('Core')->definedBy('Inventory\Core\*')
+        ->component('Adapters')->definedBy('Inventory\AdapterFor*')
+        ->component('Infrastructure')->definedBy('Inventory\Infrastructure\*')
+
+        ->where('Infrastructure')->mayDependOnComponents('Core')
+        ->where('Adapters')->mayDependOnComponents('Core', 'Infrastructure')
+        ->where('Core')->shouldNotDependOnAnyComponent()
+        ->rules();
+
+
+    $allowedDependenciesInPricingContextCoreCode = array_merge($allowedPhpDependencies, $allowedVendorDependenciesInPricingContextCore);
+    $pricingCoreIsolationRule = Rule::allClasses()
+        ->that(new ResideInOneOfTheseNamespaces('Pricing\Core'))
+        ->should(new NotHaveDependencyOutsideNamespace('Pricing\Core', $allowedDependenciesInPricingContextCoreCode))
+        ->because('we want isolate our pricing core domain from external world.');
+
+
+    $allowedDependenciesInPricingContextAdapters = array_merge($allowedPhpDependencies, $allowedVendorDependenciesInPricingContextAdapters);
+    $pricingAdaptersIsolationRule = Rule::allClasses()
+        //->except('Inventory\Adapter\Http\Web\FooController', 'Inventory\Adapter\Packagist\FooApiAdapter')
+        ->that(new ResideInOneOfTheseNamespaces('Pricing\Adapter*'))
+        ->should(new NotHaveDependencyOutsideNamespace('Pricing\Core', $allowedDependenciesInPricingContextAdapters))
+        ->because('we want isolate our pricing Adapters from ever growing dependencies.');
+
+    $config->add($pricingContextClassSet, $pricingCoreIsolationRule, $pricingAdaptersIsolationRule, ...$pricingContextPortAndAdapterArchitectureRules);
 };
--- api/src/Kernel.php
+++ api/src/Kernel.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use Cart\Infrastructure\Framework\Extension\CartModuleExtension;
+use Inventory\Infrastructure\Framework\Extension\InventoryModuleExtension;
+use Pricing\Infrastructure\Framework\Extension\PricingModuleExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container)
+    {
+        $container->registerExtension(new CartModuleExtension());
+        $container->registerExtension(new InventoryModuleExtension());
+        $container->registerExtension(new PricingModuleExtension());
+    }
 }
